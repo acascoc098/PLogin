ff69dd627075b7f55bb343a4b73b20e3df8584f8
acascoc098 <acascoc098@g.educaand.es>
2024-01-11 09:05:46 +0100

    Crear proyecto con node rama devel.

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,15 @@ const app = express();
 
 const session = require('express-session');
 // para cargar configuración de la APP desde .env
+//como los properties de java
 const dotenv = require('dotenv');
 // sistema de login y registro
 const authRoutes = require('./routes/auth');
 
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
+
+//La carpeta public tiene contenido estático
 app.use(express.static('public'));
 
 // Configuración middleware express-session
@@ -27,7 +30,15 @@ app.use((req, res, next) => {
     next();
 });
 
- // cargamos configuración desde .env
- dotenv.config();
+// cargamos configuración desde .env
+dotenv.config();
+
+app.get('/',(req,res)=>{
+    res.send('Hola Mundo');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Servidor en funcionamiento en el puerto ${process.env.SERVER_PORT}`);
+});
 
 mongoose.connect(process.env.MONGO_URI);
